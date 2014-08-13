446f4c57116ea4f52c63bd6ee6db1d48e0faebc1
octotot <octotot@gmail.com>
2014-08-14 03:33:11 +0400

    Implement views

---
+++ instacollage/CollageViewController.h
@@ -0,0 +1,13 @@
+//
+//  CollageViewController.h
+//  instacollage
+//
+//  Created by Dmitry Stankul on 14/08/14.
+//  Copyright (c) 2014 Dmitry Stankul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollageViewController : UIViewController
+
+@end
---
+++ instacollage/SelectPhotosControler.h
@@ -0,0 +1,13 @@
+//
+//  SelectPhotosControler.h
+//  instacollage
+//
+//  Created by Dmitry Stankul on 14/08/14.
+//  Copyright (c) 2014 Dmitry Stankul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SelectPhotosControler : UICollectionViewController
+
+@end
